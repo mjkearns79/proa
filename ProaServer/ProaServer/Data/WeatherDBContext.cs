f3b5717406a42dfaf3bb584f005f67299f4662f4
Mick Kearns <mick@jaix.com.au>
2024-11-17 14:36:51 +1100

    Added database context and references

---
+++ ProaServer/ProaServer/Data/WeatherDBContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProaServer.Models;
+
+namespace ProaServer.Data
+{
+    public class WeatherDbContext : DbContext
+    {
+        public DbSet<WeatherStation> WeatherStations { get; set; }
+        public DbSet<Variable> Variables { get; set; }
+        public DbSet<Measurement> Measurements { get; set; }
+        
+        public WeatherDbContext(DbContextOptions<WeatherDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<WeatherStation>().HasKey(ws => ws.Id);
+            modelBuilder.Entity<Variable>().HasKey(v => v.VarId);
+            modelBuilder.Entity<Measurement>().HasKey(m => m.Id);
+
+            modelBuilder.Entity<Variable>()
+                .HasOne<WeatherStation>()
+                .WithMany()
+                .HasForeignKey(v => v.WeatherStationId);
+
+            modelBuilder.Entity<Measurement>()
+                .HasOne<WeatherStation>()
+                .WithMany()
+                .HasForeignKey(m => m.WeatherStationId);
+
+            modelBuilder.Entity<Measurement>()
+                .HasOne<Variable>()
+                .WithMany()
+                .HasForeignKey(m => m.VarId);
+        }
+    }
+}
