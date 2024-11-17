25a858df3a9549a8d0ae8c67aaf185706f8318a2
Mick Kearns <mick@jaix.com.au>
2024-11-17 14:37:58 +1100

    Added controller

---
+++ ProaServer/ProaServer/Controllers/WeatherStationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProaServer.Data;
+
+namespace ProaServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherStationsController : ControllerBase
+    {
+        private readonly WeatherDbContext _context;
+
+        public WeatherStationsController(WeatherDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeatherStations()
+        {
+            var weatherStations = await _context.WeatherStations.Select(ws => new
+            {
+                ws.Id,
+                ws.WsName,
+                ws.Site,
+                ws.Portfolio,
+                ws.State,
+                ws.Latitude,
+                ws.Longitude
+            }).ToListAsync();
+
+            return Ok(weatherStations);
+        }
+    }
+}
